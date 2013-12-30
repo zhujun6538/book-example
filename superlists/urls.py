2c702fa9b4595ccf89254d6148a91bf328e69d4e
Harry <hjwp2@cantab.net>
2013-12-30 16:15:02 +0000

    Add accounts urls include to main urls. --ch16l004--

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
