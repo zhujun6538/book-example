e3d3081dc5acff053fc6aea5c1b23a0c794c6476
Harry <hjwp2@cantab.net>
2015-04-08 16:38:12 -0400

    fixup accounts.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login$', 'accounts.views.persona_login', name='persona_login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
-)
+]
