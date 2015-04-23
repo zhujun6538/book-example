d7ef221e5dd5a2d39dca0b58bd5d1ec58de63141
Harry <hjwp2@cantab.net>
2015-04-23 18:00:37 +0100

    first unit test for a view

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
