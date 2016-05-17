77afd46d5a6563866730ee6d1a2894a648dfa4fa
Harry <hjwp2@cantab.net>
2016-05-17 14:14:37 +0100

    completely remove all traces of the old view and disable integrated test

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
-User = get_user_model()
 
 from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import List
+
+User = get_user_model()
 
 
 def home_page(request):
@@ -11,19 +12,6 @@ def home_page(request):
 
 
 def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
-        list_ = List()
-        if request.user.is_authenticated():
-            list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
-        return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
-
-
-def new_list2(request):
     form = NewListForm(data=request.POST)
     if form.is_valid():
         list_ = form.save(owner=request.user)
